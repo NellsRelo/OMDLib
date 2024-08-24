4efb18398241566414e1089b60fb4993fa1b17a0
NellsRelo <nellsrelo@gmail.com>
2024-08-23 20:23:17 -0700

    Add UserdataToStruct() to FOMDSoftProtoPtr

--- shared/OMDLib/Types/FOMDSoftProtoPtr.lua
+++ shared/OMDLib/Types/FOMDSoftProtoPtr.lua
@@ -1,10 +1,33 @@
+FGuid = require("OMDLib.Types.FGuid")
+
 local FOMDSoftProtoPtr = {
   __index = FOMDSoftProtoPtr
 }
 
+--- Construct an FOMDSoftProtoPtr with an FGuid struct
+--- @param fguid FGuid Struct containing GUID values
+--- @return FOMDSoftProtoPtr
 function FOMDSoftProtoPtr.construct(fguid)
   local self = setmetatable({}, FOMDSoftProtoPtr)
   self.Guid = fguid
+
+  return self
+end
+
+--- Translate an OMDSoftProtoPtr from Userdata to Struct
+--- @param pointer userdata OMDSoftProtoPtr returned from the game
+--- @return FOMDSoftProtoPtr Struct to work with
+function FOMDSoftProtoPtr.UserdataToStruct(pointer)
+  local self = setmetatable({}, FOMDSoftProtoPtr)
+  -- @class FGuid
+  local guid = FGuid.construct(
+    pointer.Guid.A,
+    pointer.Guid.B,
+    pointer.Guid.C,
+    pointer.Guid.D
+  )
+
+  self = FOMDSoftProtoPtr.construct(guid)
 
   return self
 end
